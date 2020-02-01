59792ec6b86dd856232f073f710bca409b6f20b0
mrcointreau <luca.g.martini@gmail.com>
2020-02-01 14:18:33 +0100

    Add Car refactoring

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -10,6 +10,8 @@ public class Car : MonoBehaviour
     public float carLength;
     public float raycastLength;
     public LayerMask layerMask;
+
+
     private RaycastHit hit;
     private Ray ray;
 
@@ -23,22 +25,20 @@ public class Car : MonoBehaviour
     public void Fix(Part part)
     {
         parts.Remove(part);
-        if (parts.Count == 0)
-        {
-            // TODO: Next car
-        }
     }
 
     void Update()
     {
+        //Se la macchina non trova nulla davanti
         Vector3 raycastOrigin = transform.position + Vector3.right * carLength / 2;
-        if (Physics.Raycast(raycastOrigin, transform.TransformDirection(Vector3.right), out hit, raycastLength, layerMask))
+        if (!Physics.Raycast(raycastOrigin, transform.TransformDirection(Vector3.right), out hit, raycastLength, layerMask))
         {
-            Debug.Log("hit!");
-        }
-        else
-        {
-            transform.position += transform.TransformDirection(Vector3.right) * carSpeed * Time.deltaTime;
+            //Se non è arrivata a destinazione o se non è più da riparare
+            if (transform.position.x < GameManager.Instance.destinationPoint.position.x || parts.Count == 0)
+            {
+                //Vado avanti
+                transform.position += transform.TransformDirection(Vector3.right) * carSpeed * Time.deltaTime;
+            }
         }
 
     }
