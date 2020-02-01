f29b340de36c367217c57139657d53664e945aae
mrcointreau <luca.g.martini@gmail.com>
2020-02-01 19:58:07 +0100

    Add Valerio scene integration

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     #region PUBLIC VARIABLES
+    public float points = 0f;
     public Car[] availableCars;
     public List<Car> cars;
 
