7eb9e99a46a9cc002a34aec839a2c3dbdd90e13f
claudegia94 <claudegia94@libero.it>
2020-02-03 14:43:34 +0100

    Repaired car destroy

---
+++ Assets/Scripts/CarDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDestroyer : MonoBehaviour
+{
+ public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        Car carInside = other.gameObject.GetComponent<Car>();
+        if (carInside)
+        {
+            Destroy(carInside.gameObject);
+        }
+    }
+}
