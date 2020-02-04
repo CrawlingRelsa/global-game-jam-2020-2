3eceb867760a49df507c9e7111920c070e3a1247
claudegia94 <claudegia94@libero.it>
2020-02-04 21:56:42 +0100

    Added tool destroyer script and permanent tool respawn

---
+++ Assets/Scripts/ToolDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolDestroyer : MonoBehaviour
+{
+    public GameObject energyParticle;
+    public float particleScaleMultiplier = 2f;
+    public void OnTriggerEnter(Collider other)
+    {
+        Tool toolInside = other.gameObject.GetComponent<Tool>();
+        if (toolInside)
+        {
+            ParticleSystem spawnedParticles = Instantiate(energyParticle, toolInside.transform.position, this.transform.rotation).GetComponent<ParticleSystem>();
+            spawnedParticles.transform.localScale *= particleScaleMultiplier;
+            if (spawnedParticles)
+            {
+                DestroyParticle(spawnedParticles);
+            }            
+            Destroy(toolInside.gameObject);
+        }
+    }
+
+    public IEnumerator DestroyParticle(ParticleSystem particle)
+    {
+        yield return new WaitUntil(() => { return particle.isStopped;});
+        Destroy(particle.gameObject);
+    }
+}
